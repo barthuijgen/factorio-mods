a17feac255e970c58f1370c854420ca542b08391
barry <barry@barrykun.com>
2017-04-15 16:37:37 +0200

    added blueprinting, reworked building

--- autobuild/control.lua
+++ autobuild/control.lua
@@ -3,33 +3,39 @@ require("utils");
 local surface;
 local force;
 local center = {1, 1}; -- Position of center block
+local center_chest;
 local current_block_num;
 local current_block;
-local block_size = 40;
+local block_size = 100;
+local block_area;
 
--- Notes
+-- Notes:
 -- spiral pos: https://jsfiddle.net/hth00dv9/8/
 -- Inspiration: https://imgur.com/a/Ewhsv
 
-script.on_load(function(event)
-  -- start();
-end)
-script.on_init(function(event)
-  -- start();
-end)
+-- script.on_load(function(event) end)
+-- script.on_init(function(event) end)
 
-function _log(...)
-  log(serpent.block(..., {comment = false}))
+function setNextBlock()
+  current_block_num = current_block_num or -1;
+  current_block_num = current_block_num + 1;
+  current_block = blockCoord(current_block_num);
+  block_area = {
+    {current_block[1] - (block_size / 2), current_block[2] - (block_size / 2)},
+    {current_block[1] + (block_size / 2), current_block[2] + (block_size / 2)},
+  };
+  if center_chest == nil then
+    center_chest = getCenterChest();
+  end
 end
 
--- create_ghost('assembling-machine-1', {0,0});
-function create_ghost(entity_name, position)
-  surface.create_entity{
-    name = "entity-ghost",
-    position = position,
-    force = force,
-    inner_name = entity_name
-  }
+function getCenterChest()
+  local block_pos = blockCoord(0);
+  local chest = surface.find_entity("steel-chest", {current_block[1] + 0.5, current_block[2] + 0.5});
+  if chest == nil then
+    chest = buildEntity("steel-chest", current_block, {force_build = true});
+  end
+  return chest;
 end
 
 function getBlueprintArea(bp)
@@ -56,9 +62,10 @@ end
 function setTileArea(name, area)
   -- "grass-dry" / "sand-dark"
   tiles = {};
-  for x=math.floor(area[1][1]), math.ceil(area[2][1]), 1 do
-    for y=math.floor(area[1][2]), math.ceil(area[2][2]), 1 do
-      if surface.get_tile(x, y).name ~= name then
+  for x=math.floor(area[1][1]) - 1, math.ceil(area[2][1]), 1 do
+    for y=math.floor(area[1][2]) -1, math.ceil(area[2][2]), 1 do
+      local tile = surface.get_tile(x, y);
+      if tile.valid and tile.name ~= name then
         table.insert(tiles, {
           name = name,
           position = {x, y}
@@ -68,13 +75,6 @@ function setTileArea(name, area)
   end
   surface.set_tiles(tiles);
   return tiles;
-end
-
-function getAreaAtPostion(area, position)
-  return {
-    {area[1][1] + position[1], area[1][2] + position[2]},
-    {area[2][1] + position[1], area[2][2] + position[2]}
-  };
 end
 
 function blockCoord(n)
@@ -109,73 +109,33 @@ function buildGhostEntity(name, pos, data)
     name = "entity-ghost",
     inner_name = name
   });
-  buildEntity(name, pos, data);
+  return buildEntity(name, pos, data);
 end
 
 function buildEntity(name, position, data)
-  surface.create_entity(mergeTables({
+  local args = mergeTables({
     name = name,
     position = position,
     force = force
-  }, data))
+  }, data);
+  if surface.can_place_entity(args) or data.force_build then
+    return surface.create_entity(args);
+  end
 end
 
-function buildRailAroundArea(area)
-  -- local curner_cutoff = 11; -> to make rounded corners
-  local curner_cutoff = 0;
-  local x = area[1][1] + curner_cutoff;
-  local y = area[1][2];
-  -- top line
-  while x < area[2][1] - curner_cutoff do
-    buildEntity("straight-rail", {x,y}, {direction = 2});
-    x = x + 2;
-  end
-  x = x + curner_cutoff;
-  -- right line
-  while y < area[2][2] do
-    buildEntity("straight-rail", {x,y}, {direction = 0});
-    y = y + 2;
-  end
-  -- bottom line
-  while x > area[1][1] do
-    buildEntity("straight-rail", {x,y}, {direction = 2});
-    x = x - 2;
-  end
-  -- left line
-  while y > area[1][2] do
-    buildEntity("straight-rail", {x,y}, {direction = 0});
-    y = y - 2;
-  end
-  -- top-left corner
-  buildEntity("curved-rail", {area[1][1] + 1, area[1][2] + 7}, {direction = 1});
-  buildEntity("curved-rail", {area[1][1] + 7, area[1][2] + 1}, {direction = 6});
-  buildEntity("straight-rail", {area[1][1] + 4, area[1][2] + 4}, {direction = 7});
-  -- top-right corner
-  buildEntity("curved-rail", {area[2][1] - 1, area[1][2] + 7}, {direction = 0});
-  buildEntity("curved-rail", {area[2][1] - 7, area[1][2] + 1}, {direction = 3});
-  buildEntity("straight-rail", {area[2][1] - 4, area[1][2] + 4}, {direction = 1});
-  -- bottom-left corner
-  buildEntity("curved-rail", {area[1][1] + 1, area[2][2] - 7}, {direction = 4});
-  buildEntity("curved-rail", {area[1][1] + 7, area[2][2] - 1}, {direction = 7});
-  buildEntity("straight-rail", {area[1][1] + 4, area[2][2] - 4}, {direction = 5});
-  -- bottom-right corner
-  buildEntity("curved-rail", {area[2][1] - 1, area[2][2] - 7}, {direction = 5});
-  buildEntity("curved-rail", {area[2][1] - 7, area[2][2] - 1}, {direction = 2});
-  buildEntity("straight-rail", {area[2][1] - 4, area[2][2] - 4}, {direction = 3});
+function makeBlueprintEntity(num, name, position, data)
+  return mergeTables({
+    entity_number = num,
+    name = name,
+    position = position,
+  }, data);
 end
+
+
 
 function buildBlock()
-  -- Calculate block area
-  local block_area = {
-    {current_block[1], current_block[2]},
-    {current_block[1] + block_size, current_block[2] + block_size},
-  };
-
   -- Clear ground floor
-  setTileArea("grass-dry", block_area);
-
-  -- Mark the area
-  buildRailAroundArea(block_area);
+  setTileArea("grass", block_area);
 
   -- Find out what is in this block
   local block_entities = {};
@@ -193,8 +153,43 @@ function buildBlock()
   _log("Block " .. current_block_num .. " (" .. current_block[1] .. "," .. current_block[2] .. "): ");
   _log(block_entities);
 
-  current_block_num = current_block_num + 1;
-  current_block = blockCoord(current_block_num);
+  if center_chest ~= nil then
+    -- Find build chest blueprints
+    local invt = center_chest.get_inventory(defines.inventory.chest);
+    -- Insert blueprints if there are too little
+    for i = invt.get_item_count("blueprint"), 5, 1 do
+      invt.insert{name="blueprint"};
+    end
+    local blueprint = invt[1];
+    if blueprint.name == 'blueprint' then
+      if blueprint.is_blueprint_setup() ~= true then
+        blueprint.set_blueprint_entities(getBlueprintRailEntities(block_size));
+      end
+      buildBlueprint(blueprint, true);
+    end
+  end
+
+  setNextBlock();
+end
+
+function buildBlueprint(blueprint, autobuild)
+  blueprint.build_blueprint({
+    surface = surface,
+    force = force,
+    position = current_block,
+    force_build = true,
+    direction = defines.direction.north
+  });
+
+  force.chart(surface, block_area);
+  
+  if autobuild == true then
+    for _, entity in pairs(surface.find_entities(block_area)) do
+      if entity.name == "entity-ghost" then
+        entity.revive();
+      end
+    end
+  end
 end
 
 script.on_event(defines.events.on_put_item, function(event)
@@ -202,14 +197,14 @@ script.on_event(defines.events.on_put_item, function(event)
 end)
 
 script.on_event(defines.events.on_tick, function(event)
-  if event.tick % 120 == 0 then
-    if surface == nil then
-      surface = game.surfaces['nauvis'];
-      force = game.players[1].force;
-      current_block_num = 0;
-      current_block = blockCoord(current_block_num);
-    end
+  -- This will move to globals later
+  if surface == nil then
+    surface = game.surfaces['nauvis'];
+    force = game.players[1].force;
+    setNextBlock();
+  end
 
+  if event.tick % 120 == 0 then
     -- buildBlock();
 
     -- local bp = game.players[1].get_quickbar()[1];
--- autobuild/utils.lua
+++ autobuild/utils.lua
@@ -1,3 +1,7 @@
+function _log(...)
+  log(serpent.block(..., {comment = false}))
+end
+
 function mergeTables(t1, t2)
   if t1 == nil then
     t1 = {};
@@ -9,4 +13,126 @@ function mergeTables(t1, t2)
     t1[k] = v
   end
   return t1;
+end
+
+Blueprint = {index = 0, entities = {}}
+
+function Blueprint:new (o)
+  o = o or {}   -- create object if user does not provide one
+  setmetatable(o, self)
+  self.__index = self
+  self.index = 1;
+  self.entities = {};
+  return o;
+end
+
+function Blueprint:add(entity)
+  table.insert(self.entities, mergeTables({
+    entity_number = self.index
+  }, entity));
+  self.index = self.index + 1;
+end
+
+function Blueprint:get_entities()
+  return self.entities;
+end
+
+function getRailTurn(position, direction)
+  local curve_start = {name="curved-rail", position=position};
+  local curve_middle = {name="straight-rail", position=position};
+  local curve_end = {name="curved-rail", position=position};
+  if (direction == "top-left") then
+    curve_start.position = { curve_start.position[1] + 1, curve_start.position[2] + 7};
+    curve_middle.position = { curve_middle.position[1] + 4, curve_middle.position[2] + 4};
+    curve_end.position = { curve_end.position[1] + 7, curve_end.position[2] + 1};
+    curve_start.direction = 1;
+    curve_middle.direction = 7;
+    curve_end.direction = 6;
+  end
+  if (direction == "top-right") then
+    curve_start.position = { curve_start.position[1] - 7, curve_start.position[2] + 1};
+    curve_middle.position = { curve_middle.position[1] - 4, curve_middle.position[2] + 4};
+    curve_end.position = { curve_end.position[1] - 1, curve_end.position[2] + 7};
+    curve_start.direction = 3;
+    curve_middle.direction = 1;
+    curve_end.direction = 0;
+  end
+  if (direction == "bottom-left") then
+    curve_start.position = { curve_start.position[1] + 7, curve_start.position[2] - 1};
+    curve_middle.position = { curve_middle.position[1] + 4, curve_middle.position[2] - 4};
+    curve_end.position = { curve_end.position[1] + 1, curve_end.position[2] - 7};
+    curve_start.direction = 7;
+    curve_middle.direction = 5;
+    curve_end.direction = 4;
+  end
+  if (direction == "bottom-right") then
+    curve_start.position = { curve_start.position[1] - 1, curve_start.position[2] - 7};
+    curve_middle.position = { curve_middle.position[1] - 4, curve_middle.position[2] - 4};
+    curve_end.position = { curve_end.position[1] - 7, curve_end.position[2] - 1};
+    curve_start.direction = 5;
+    curve_middle.direction = 3;
+    curve_end.direction = 2;
+
+  end
+  return {curve_start, curve_middle, curve_end};
+end
+
+function getBlueprintRailEntities(block_size)
+  local bp = Blueprint:new();
+  local radius = (block_size / 2);
+  local area = {{-radius, -radius},{radius,radius}};
+  local x = -(block_size / 2);
+  local y = -(block_size / 2);
+  -- top line
+  while x < area[2][1] do
+    bp:add({name = "straight-rail", position = {x,y}, direction = 2});
+    x = x + 2;
+  end
+  -- right line
+  while y < area[2][2] do
+    bp:add({name = "straight-rail", position = {x,y}, direction = 0});
+    y = y + 2;
+  end
+  -- bottom line
+  while x > area[1][1] do
+    bp:add({name = "straight-rail", position = {x,y}, direction = 2});
+    x = x - 2;
+  end
+  -- left line
+  while y > area[1][2] do
+    bp:add({name = "straight-rail", position = {x,y}, direction = 0});
+    y = y - 2;
+  end
+  for _, entity in pairs(getRailTurn({area[1][1], area[1][2]}, "top-left")) do
+    bp:add(entity);
+  end
+  for _, entity in pairs(getRailTurn({area[2][1], area[1][2]}, "top-right")) do
+    bp:add(entity);
+  end
+  for _, entity in pairs(getRailTurn({area[1][1], area[2][2]}, "bottom-left")) do
+    bp:add(entity);
+  end
+  for _, entity in pairs(getRailTurn({area[2][1], area[2][2]}, "bottom-right")) do
+    bp:add(entity);
+  end
+  -- EXTRA items
+  bp:add({name = "roboport", position = {-radius/2,-radius/2}});
+  bp:add({name = "roboport", position = {radius/2,-radius/2}});
+  bp:add({name = "roboport", position = {-radius/2,radius/2}});
+  bp:add({name = "roboport", position = {radius/2,radius/2}});
+  return bp:get_entities();
+end
+
+function getAreaAtPostion(area, position)
+  return {
+    {area[1][1] + position[1], area[1][2] + position[2]},
+    {area[2][1] + position[1], area[2][2] + position[2]}
+  };
+end
+
+function getAreaCenter(area)
+  return {
+    area[2][1] - (area[2][1] - area[1][1] / 2),
+    area[2][2] - (area[2][2] - area[1][2] / 2),
+  };
 end
