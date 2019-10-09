12931cc351f943f75f52a5b72ec8a995772495d3
Robert Krauße <robert.krausse@axetrading.com>
2019-10-09 19:26:05 +0200

    Update settings.lua

--- unlimited-resources/settings.lua
+++ unlimited-resources/settings.lua
@@ -1,31 +1,24 @@
 data:extend({
   {
-    name = "resource-refill-amount",
+    name = "resource-refill-amount-ore",
     type = "int-setting",
     setting_type = "runtime-global",
     default_value = 100000,
     order = "a"
   },
   {
-    name = "resource-oil-refill-amount",
+    name = "resource-refill-amount-oil",
     type = "int-setting",
     setting_type = "runtime-global",
     default_value = 100,
     order = "b"
   },
   {
-    name = "resource-refill-interval",
+    name = "resource-refill-interval-oil",
     type = "int-setting",
     setting_type = "runtime-global",
     default_value = 300,
     minimum_value = 5,
     order = "c"
-  },
-  {
-    name = "resource-refill-disabled",
-    type = "bool-setting",
-    setting_type = "runtime-global",
-    default_value = true,
-    order = "d"
   },
 });
