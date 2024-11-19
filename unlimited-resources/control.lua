160a142fc510c058bcf99432915e6e63addfc5a0
Bart Huijgen <barthuijgen@users.noreply.github.com>
2024-11-19 14:10:58 +0100

    Unlimited resources update to 1.0.11

--- unlimited-resources/control.lua
+++ unlimited-resources/control.lua
@@ -1,77 +1,125 @@
-local firstRefill = false;
-local refillAmount = settings.global["resource-refill-amount"].value;
+local refillAmount = settings.global["resource-ore-refill-amount"].value;
 local refillAmountOil = settings.global["resource-oil-refill-amount"].value * 3000; -- percentage times 3000
 local refillInterval = settings.global["resource-refill-interval"].value * 60; -- ticks (18000 = 5min)
-local refillDisabled = settings.global["resource-refill-disabled"].value;
+local refillOreEnabled = settings.global["resource-refill-ore-enabled"].value;
+local refillOilEnabled = settings.global["resource-refill-oil-enabled"].value;
+-- Iterator memory to spread refill out over multiple ticks
+local refillActive = false;
+local currentSurface = nil;
+local currentChunk = nil;
 
-function _log(...)
-  log(serpent.block(..., {comment = false}))
+function _print(...)
+    local args = {
+        n = select("#", ...),
+        ...
+    };
+    local string = "";
+    for i, player in pairs(game.players) do
+        for i = 1, args.n do
+            string = string .. serpent.block(args[i]) .. "\t";
+        end
+        player.print(string);
+    end
 end
 
-script.on_init(function()
-  firstRefill = true;
-end);
-
-function refillResources(resources)
-  for _, entity in pairs(resources) do
-    if entity.name == "crude-oil" then
-      entity.amount = refillAmountOil;
-    else
-      entity.amount = refillAmount;
+function refillEntity(entity)
+    if refillOreEnabled and entity.prototype.resource_category == "basic-solid" then
+        entity.amount = refillAmount;
     end
-  end
+    if refillOilEnabled and entity.prototype.resource_category == "basic-fluid" and entity.prototype.infinite_resource ==
+        true then
+        entity.amount = refillAmountOil;
+    end
+end
+
+function refillResourceArea(surface, area)
+    local resources = surface.find_entities_filtered({
+        area = area,
+        type = "resource"
+    });
+    for _, entity in pairs(resources) do
+        refillEntity(entity);
+    end
+end
+
+function getCurrentSurface()
+    local index = 1;
+    for _, surface in pairs(game.surfaces) do
+        if currentSurface == index then
+            return surface;
+        end
+        index = index + 1
+    end
+    return nil
+end
+
+function getNextChunks(surface, amount)
+    local chunkList = {};
+    local listIndex = 1;
+    local index = 1;
+    for chunk in surface.get_chunks() do
+        if currentChunk <= index then
+            chunkList[listIndex] = chunk;
+            listIndex = listIndex + 1;
+        end
+        if listIndex > amount then
+            return chunkList;
+        end
+        index = index + 1;
+    end
+    return chunkList;
 end
 
 function refillAllResources()
-  -- Warning! this will cause issues on multiplayer, disabled is true by default
-  for _, surface in pairs(game.surfaces) do
-    refillResources(surface.find_entities_filtered({
-      type = "resource"
-    }));
-  end
+    local surface = getCurrentSurface();
+    if not surface then
+        return true
+    end
+    local chunkList = getNextChunks(surface, 50);
+
+    if #chunkList == 0 then
+        currentChunk = 1;
+        currentSurface = currentSurface + 1;
+        return refillAllResources();
+    end
+
+    for _, chunk in pairs(chunkList) do
+        refillResourceArea(surface, chunk.area);
+    end
+
+    currentChunk = currentChunk + #chunkList;
+    return false
 end
 
 function onTick(event)
-  if firstRefill then
-    firstRefill = false;
-    refillAllResources();
-  elseif event.tick % refillInterval == 0 then
-    refillAllResources();
-  end
+    if not refillActive and (refillOreEnabled or refillOilEnabled) and event.tick % refillInterval == 0 then
+        refillActive = true;
+        currentSurface = 1;
+        currentChunk = 1;
+    end
+
+    if refillActive then
+        local finished = refillAllResources()
+        if finished then
+            refillActive = false;
+        end
+    end
 end
 
 function onChunkGenerated(event)
-  refillResources(event.surface.find_entities_filtered({
-    area = event.area,
-    type = "resource"
-  }));
+    refillResourceArea(event.surface, event.area);
 end
 
 function onResourceDepleted(event)
-  if event.entity.name == "crude-oil" then
-    event.entity.amount = refillAmountOil;
-  else
-    event.entity.amount = refillAmount;
-  end
+    refillEntity(event.entity)
 end
 
 script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
-  local changed = (refillDisabled ~= settings.global["resource-refill-disabled"].value);
-  refillAmount = settings.global["resource-refill-amount"].value;
-  refillAmountOil = settings.global["resource-oil-refill-amount"].value * 3000;
-  refillInterval = settings.global["resource-refill-interval"].value * 60;
-  refillDisabled = settings.global["resource-refill-disabled"].value;
-  if changed or refillDisabled == false then
-    if refillDisabled and changed then
-      script.on_event(defines.events.on_tick, nil);
-    elseif changed then
-      script.on_event(defines.events.on_tick, onTick);
-    end
-    -- Refill if allowed
-    if refillDisabled == false then
-      refillAllResources();
-    end
-  end
+    refillAmount = settings.global["resource-ore-refill-amount"].value;
+    refillAmountOil = settings.global["resource-oil-refill-amount"].value * 3000;
+    refillInterval = settings.global["resource-refill-interval"].value * 60;
+    refillOreEnabled = settings.global["resource-refill-ore-enabled"].value;
+    refillOilEnabled = settings.global["resource-refill-oil-enabled"].value;
 end)
 
 -- Increase resource amount when generated
@@ -79,8 +127,5 @@ script.on_event(defines.events.on_chunk_generated, onChunkGenerated);
 -- Refill resources on depletion
 script.on_event(defines.events.on_resource_depleted, onResourceDepleted);
 
-if refillDisabled == false then
-  -- Warning! this will cause issues on multiplayer, disabled is true by default
-  -- Loop to refill on interval
-  script.on_event(defines.events.on_tick, onTick);
-end
+-- Refill on interval (if enabled)
+script.on_event(defines.events.on_tick, onTick);
--- unlimited-resources/settings.lua
+++ unlimited-resources/settings.lua
@@ -1,31 +1,32 @@
-data:extend({
-  {
-    name = "resource-refill-amount",
+data:extend({{
+    name = "resource-ore-refill-amount",
     type = "int-setting",
     setting_type = "runtime-global",
     default_value = 100000,
     order = "a"
-  },
-  {
+}, {
     name = "resource-oil-refill-amount",
     type = "int-setting",
     setting_type = "runtime-global",
     default_value = 100,
     order = "b"
-  },
-  {
+}, {
     name = "resource-refill-interval",
     type = "int-setting",
     setting_type = "runtime-global",
     default_value = 300,
     minimum_value = 5,
     order = "c"
-  },
-  {
-    name = "resource-refill-disabled",
+}, {
+    name = "resource-refill-ore-enabled",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "d"
+}, {
+    name = "resource-refill-oil-enabled",
     type = "bool-setting",
     setting_type = "runtime-global",
     default_value = true,
-    order = "d"
-  },
-});
+    order = "e"
+}});
